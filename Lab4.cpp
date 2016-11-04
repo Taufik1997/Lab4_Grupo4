9f3d60efb6e7c325f168f746a5e716764a4a5741
Taufik Azzad <taufikabad@unitec.edu>
2016-11-04 16:05:01 -0600

    f-u

--- Lab4.cpp
+++ Lab4.cpp
@@ -51,7 +51,7 @@ int opcion = 0;
 							caraSuperior[2][i] = numero1;
 							caraDerecha[i][0] = numero2;
 							caraInferior[2][i] = numero3;
-							caraIzquierda[i][0] = numero4;
+							caraIzquierda[i][2] = numero4;
 						}
 					}
 					if(movimiento == "f"){
@@ -65,10 +65,98 @@ int opcion = 0;
 							caraSuperior[2][i] = numero3;
 							caraDerecha[i][0] = numero4;
 							caraInferior[2][i] = numero1;
+							caraIzquierda[i][2] = numero2;
+						}
+					}
+					if(movimiento == "X"){
+						int numero1,numero2,numero3,numero4;
+						for (int i = 0; i < 3; ++i)
+						{
+							numero1 = caraIzquierda[i][0];
+							numero2 =caraSuperior[0][i];
+							numero3 =caraDerecha[i][2];
+							numero4 =caraInferior[0][i];
+							caraSuperior[0][i] = numero1;
+							caraDerecha[i][2] = numero2;
+							caraInferior[0][i] = numero3;
+							caraIzquierda[i][0] = numero4;
+						}
+					}
+					if(movimiento == "x"){
+						int numero1,numero2,numero3,numero4;
+						for (int i = 0; i < 3; ++i)
+						{
+							numero1 = caraIzquierda[i][0];
+							numero2 =caraSuperior[0][i];
+							numero3 =caraDerecha[i][2];
+							numero4 =caraInferior[0][i];
+							caraSuperior[0][i] = numero3;
+							caraDerecha[i][2] = numero4;
+							caraInferior[0][i] = numero1;
 							caraIzquierda[i][0] = numero2;
 						}
 					}
 
+					if(movimiento == "U"){
+						int numero1,numero2,numero3,numero4;
+
+						for (int i = 0; i < 3; ++i)
+						{
+							numero1 = caraIzquierda[0][i];
+							numero2 =caraFrontal[0][i];
+							numero3 =caraDerecha[0][i];
+							numero4 =caraTrasera[0][i];
+							caraFrontal[0][i] = numero1;
+							caraDerecha[0][i] = numero2;
+							caraTrasera[0][i] = numero3;
+							caraIzquierda[0][i] = numero4;
+						}
+					}
+					if(movimiento == "u"){
+						int numero1,numero2,numero3,numero4;
+
+						for (int i = 0; i < 3; ++i)
+						{
+							numero1 = caraIzquierda[0][i];
+							numero2 =caraFrontal[0][i];
+							numero3 =caraDerecha[0][i];
+							numero4 =caraTrasera[0][i];
+							caraFrontal[0][i] = numero3;
+							caraDerecha[0][i] = numero4;
+							caraTrasera[0][i] = numero1;
+							caraIzquierda[0][i] = numero2;
+						}
+					}
+					if(movimiento == "B"){
+						int numero1,numero2,numero3,numero4;
+
+						for (int i = 0; i < 3; ++i)
+						{
+							numero1 = caraIzquierda[2][i];
+							numero2 =caraFrontal[2][i];
+							numero3 =caraDerecha[2][i];
+							numero4 =caraTrasera[2][i];
+							caraFrontal[2][i] = numero1;
+							caraDerecha[2][i] = numero2;
+							caraTrasera[2][i] = numero3;
+							caraIzquierda[2][i] = numero4;
+						}
+					}
+					if(movimiento == "b"){
+						int numero1,numero2,numero3,numero4;
+
+						for (int i = 0; i < 3; ++i)
+						{
+							numero1 = caraIzquierda[2][i];
+							numero2 =caraFrontal[2][i];
+							numero3 =caraDerecha[2][i];
+							numero4 =caraTrasera[2][i];
+							caraFrontal[2][i] = numero3;
+							caraDerecha[2][i] = numero4;
+							caraTrasera[2][i] = numero1;
+							caraIzquierda[2][i] = numero2;
+						}
+					}
 
 
 					cout<< "Cara Frontal"<<endl;
