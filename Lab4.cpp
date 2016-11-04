065f348b5785c7ce09752749340f447bac94c39f
Jorge Morazan <jorgemorazan@unitec.edu>
2016-11-04 15:28:52 -0600

    mensaje

--- Lab4.cpp
+++ Lab4.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int** crearMatriz(int x);
-void imprimirMatriz(int** matriz);
+int** crearMatriz(int);
+void imprimirMatriz(int**);
 
 int main(){
 
@@ -15,6 +15,7 @@ int** caraInferior = crearMatriz(4);
 int** caraDerecha = crearMatriz(5);
 int** caraIzquierda = crearMatriz(6);
 
+imprimirMatriz(caraFrontal);
 int opcion = 0;
 	do{
 		cout<<"Menu\n1)Normal\n2)Scramble\n3)Salir\n";
@@ -29,26 +30,31 @@ int opcion = 0;
 				break;
 			}
 		}//Fin Switch 
-	}while(opcion>=0 && opcion <=3);//Fin do while
+	}while(opcion!=3);//Fin do while
 	return 0;
 }
 
 int** crearMatriz(int x){
 	int **matriz;
 	matriz = new int*[3];
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int i = 0; j < 3; ++j)
-		{
-			matriz[i][j]= x;
-		}
-	}
+	int size=3;
+	for (int i = 0; i < size; ++i)
+    {
+      matriz[i]=new int[size];
+    }
+    for (int i = 0; i < size; ++i)
+    {
+      for (int j = 0; j < size; ++j)
+      {
+        matriz[i][j]=x;
+      }
+    }
 
 	return matriz;
 }
 
 void imprimirMatriz(int** matriz){
-	for (int i = 0; i < 3; ++j)
+	for (int i = 0; i < 3; ++i)
 	{
 		for (int j = 0; j < 3; ++j)
 		{
